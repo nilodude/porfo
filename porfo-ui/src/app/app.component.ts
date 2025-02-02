ba2ecea6ee69fd5233ed913fe8f5a18301dee73f
nilodude <alphone@protonmail.com>
2025-02-02 20:47:47 +0100

    tambien guapo, el anterior puede que mejor

--- porfo-ui/src/app/app.component.ts
+++ porfo-ui/src/app/app.component.ts
@@ -7,7 +7,7 @@ const sketch = (p: p5) => {
 
   p.setup = () => {
     p.createCanvas(p.windowWidth, p.windowHeight);
-    p.background(120);
+    p.background(150);
   };
 
   p.windowResized = () => {
@@ -24,13 +24,13 @@ const sketch = (p: p5) => {
   let x = margin;
   let y = 0;
   let flip =false
-  let step = 2
+  let step = 15
   let capao = 80
   p.draw = () => {
     console.log(p.width , x ,flip)
     
-    p.fill(50+Math.random()*220,50+Math.random()*220,50+Math.random()*220,Math.random()*50)
-    p.stroke(Math.random()*100,Math.random()*100,Math.random()*100,Math.random()*70)
+    p.fill(Math.random()*220,50+Math.random()*220,50+Math.random()*220,Math.random()*40)
+    p.stroke(Math.random()*100,Math.random()*100,Math.random()*100,Math.random()*30)
     
     let w = (x/20)*y/900
     let h = (y*y/3);
@@ -44,7 +44,7 @@ const sketch = (p: p5) => {
       flip = !flip
     }
     // p.curve(x, y+500 ,x+w ,y+500,x, y+500 ,x+500*y/20 ,y+h+500)
-    p.curve(x, y+Math.random()*500 ,x+w ,y+Math.random()*500,x, y+Math.random()*500 ,x+Math.random()*500*y/20 ,y+h+Math.random()*500)
+    p.curve(x, y+Math.random()*500 ,x+w ,y+Math.random()*500,x, y+Math.random()*700 ,x+Math.random()*500*y/20 ,y+h+Math.random()*500)
   };
 };
 
