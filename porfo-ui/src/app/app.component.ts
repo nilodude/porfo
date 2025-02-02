d21212f0f891ad82914cc3439fe42479d7d8db55
nilodude <alphone@protonmail.com>
2025-02-02 20:21:22 +0100

    10000€

--- porfo-ui/src/app/app.component.ts
+++ porfo-ui/src/app/app.component.ts
@@ -7,15 +7,13 @@ const sketch = (p: p5) => {
 
   p.setup = () => {
     p.createCanvas(p.windowWidth, p.windowHeight);
-    p.background(220);
-    p.line(0, 0, p.windowWidth, p.windowWidth);
+    p.background(120);
   };
 
   p.windowResized = () => {
     
     p.resizeCanvas(p.windowWidth, p.windowHeight);
     p.background(220);
-    p.line(0, 0, p.windowWidth, p.windowWidth);
   };
   let colors = [
     'dedeff',
@@ -31,13 +29,13 @@ const sketch = (p: p5) => {
   p.draw = () => {
     console.log(p.width , x ,flip)
     
-    p.fill(50+Math.random()*120,50+Math.random()*120,50+Math.random()*120,Math.random()*50)
+    p.fill(50+Math.random()*220,50+Math.random()*220,50+Math.random()*220,Math.random()*50)
     p.stroke(Math.random()*100,Math.random()*100,Math.random()*100,Math.random()*70)
     
     let w = (x/20)*y/900
     let h = (y*y/3);
     h = h> capao ? capao : (h<-capao ? -capao : h)
-    p.ellipse(x, y+ p.height / 2 ,w ,h);
+    // p.ellipse(x, y+ p.height / 2 ,w ,h);
     
     x = x + step*(flip?-1:1)+w*(Math.random()-0.5);
     y += h/10000+(Math.random()-0.5) *-3
@@ -45,7 +43,7 @@ const sketch = (p: p5) => {
     if(Math.abs(p.windowWidth -x) < margin || x<margin ){
       flip = !flip
     }
-    p.curve(x, y+500 ,x+w ,y+500,x, y+500 ,x+500*y/20 ,y+h+500)
+    // p.curve(x, y+500 ,x+w ,y+500,x, y+500 ,x+500*y/20 ,y+h+500)
     p.curve(x, y+Math.random()*500 ,x+w ,y+Math.random()*500,x, y+Math.random()*500 ,x+Math.random()*500*y/20 ,y+h+Math.random()*500)
   };
 };
