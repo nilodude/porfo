bebceb01257841f8010ce7c03e0bded664a49c85
nilodude <alphone@protonmail.com>
2025-02-03 20:25:27 +0100

    div overlaying sketch OK

--- porfo-ui/src/app/app.component.ts
+++ porfo-ui/src/app/app.component.ts
@@ -4,15 +4,15 @@ import p5 from 'p5';
 
 const sketch = (p: p5) => {
   p.preload = () => {};
-
+  const appMargin = {x:0,y:5}
   p.setup = () => {
-    p.createCanvas(p.windowWidth, p.windowHeight);
+    p.createCanvas(p.windowWidth-appMargin.x, p.windowHeight-appMargin.y, 'webgl');
     // p.background(120);
   };
 
   p.windowResized = () => {
     
-    p.resizeCanvas(p.windowWidth, p.windowHeight);
+    p.resizeCanvas(p.windowWidth-appMargin.x, p.windowHeight-appMargin.y);
     // p.background(220);
   };
   let colors = [
@@ -20,27 +20,31 @@ const sketch = (p: p5) => {
     '#cbffcb',
     '#cbdeff',
     '#122131']
-  let margin = 300
+  let margin = 100
   let x = margin;
   let y = 0;
-  let flip =false
+  let flipX =false
+  let flipY =false
   let step = 10
   let capao = 80
   p.draw = () => {
     
-    p.fill(180,170,200,Math.random()*70)
-    p.stroke(Math.random()*100,Math.random()*100,Math.random()*100,Math.random()*100)
+    p.fill(180,170,200,Math.random()*50)
+    p.stroke(100,Math.random()*80,200,Math.random()*100)
     
-    let w = (x/20)*y/900
+    let w = (x/40)*y/10000
     let h = (y*y/3);
     h = h> capao ? capao : (h<-capao ? -capao : h)
-    p.ellipse(x, y+ p.height / 2 ,w ,h);
-      
-    x = x + step*(flip?-1:1)+w*(Math.random()-0.5);
-    y += h/1000+(Math.random()-0.5) *-5
+    p.ellipse(x-p.width/2, y ,w ,h);
+    p.rotateZ(w)
+    x = x + step*(flipX?-1:1)+w*(Math.random()-0.5);
+    y =y + (flipX?-1:1)*h/400+(Math.random()-0.5) *-28
     
     if(Math.abs(p.windowWidth -x) < margin || x<margin ){
-      flip = !flip
+      flipX = !flipX
+    }
+    if(Math.abs(p.windowHeight -y) < margin || y<margin ){
+      flipY = !flipY
     }
       
   };
