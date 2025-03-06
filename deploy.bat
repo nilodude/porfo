1720b804ef4cf8a08d60e9bb29db0d5ce12abc9a
nilodude <alphone@protonmail.com>
2025-03-06 21:15:06 +0100

    cursor. still need to fix background-text contrast

--- deploy.bat
+++ deploy.bat
@@ -1,3 +1,5 @@
 call npm run build --prefix porfo-ui
 call docker build --tag porfo-ui ./porfo-ui
 call docker compose -f docker-compose-win.yml -p porfo --env-file .env.pro up -d
+
+call npm run deploy --prefix porfo-ui
